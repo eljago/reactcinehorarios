cbe36c629d9f29fe8f60c995d35b5ba2b3e58a1a
Arturo Espinoza <clonjago@gmail.com>
2016-04-07 23:25:09 -0300

    added codepush

--- ios/CineHorarios/AppDelegate.m
+++ ios/CineHorarios/AppDelegate.m
@@ -11,6 +11,8 @@
 
 #import "RCTRootView.h"
 
+#import "CodePush.h"
+
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
@@ -31,7 +33,7 @@
    * on the same Wi-Fi network.
    */
 
-  jsCodeLocation = [NSURL URLWithString:@"http://localhost:8081/index.ios.bundle?platform=ios&dev=true"];
+//  jsCodeLocation = [NSURL URLWithString:@"http://192.168.50.17:8081/index.ios.bundle?platform=ios&dev=true"];
 
   /**
    * OPTION 2
@@ -40,6 +42,15 @@
    */
 
 //   jsCodeLocation = [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
+  
+  /**
+   * CODE-PUSH
+   */
+#ifdef DEBUG
+  jsCodeLocation = [NSURL URLWithString:@"http://192.168.50.17:8081/index.ios.bundle?platform=ios&dev=true"];
+#else
+  jsCodeLocation = [CodePush bundleURL];
+#endif
 
   RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
                                                       moduleName:@"CineHorarios"
